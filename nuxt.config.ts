2f6763bb54929b05aec2c319c9d62137effff52b
Vordgi <sasha2822222@gmail.com>
2026-02-23 20:32:02 +0000

    fix: check payload fallback with exact rule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -134,7 +134,10 @@ export default defineNuxtConfig({
     },
     // pages
     '/package/**': getISRConfig(60, { fallback: 'html' }),
-    '/package/**/_payload.json': getISRConfig(60, { fallback: 'json' }),
+    '/package/:name/_payload.json': getISRConfig(60, { fallback: 'json' }),
+    '/package/:name/v/:version/_payload.json': getISRConfig(60, { fallback: 'json' }),
+    '/package/:org/:name/_payload.json': getISRConfig(60, { fallback: 'json' }),
+    '/package/:org/:name/v/:version/_payload.json': getISRConfig(60, { fallback: 'json' }),
     // infinite cache (versioned - doesn't change)
     '/package-code/**': { isr: true, cache: { maxAge: 365 * 24 * 60 * 60 } },
     '/package-docs/**': { isr: true, cache: { maxAge: 365 * 24 * 60 * 60 } },
