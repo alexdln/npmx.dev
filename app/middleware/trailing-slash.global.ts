a6a922dfa6f9cc6e008083283a15bbf48c8e1473
Vordgi <sasha2822222@gmail.com>
2026-02-23 15:00:27 +0000

    fix: disable trailing slash middleware

--- app/middleware/trailing-slash.global.ts
+++ app/middleware/trailing-slash.global.ts
@@ -7,22 +7,23 @@
  * - /package/vue/ → /package/vue
  * - /docs/getting-started/?query=value → /docs/getting-started?query=value
  */
-export default defineNuxtRouteMiddleware(to => {
-  if (import.meta.server) {
-    const event = useRequestEvent()
-    const url = event?.node.req.originalUrl || event?.node.req.url || ''
+export default defineNuxtRouteMiddleware(() => {
+  // if (import.meta.server) {
+  //   const event = useRequestEvent()
+  //   const url = event?.node.req.originalUrl || event?.node.req.url || ''
 
-    if (url.includes('_payload')) return
-  }
+  //   if (url.includes('_payload')) return
+  // }
 
-  if (to.path !== '' && !to.path.endsWith('/')) {
-    return navigateTo(
-      {
-        path: to.path + '/',
-        query: to.query,
-        hash: to.hash,
-      },
-      { replace: true },
-    )
-  }
+  // if (to.path !== '' && !to.path.endsWith('/')) {
+  //   return navigateTo(
+  //     {
+  //       path: to.path + '/',
+  //       query: to.query,
+  //       hash: to.hash,
+  //     },
+  //     { replace: true },
+  //   )
+  // }
+  return
 })
