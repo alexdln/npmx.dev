dbcffe161669bfc8abc0bd7c1d71ac3082613750
Vordgi <sasha2822222@gmail.com>
2026-02-23 13:38:09 +0000

    fix: improve trailing slash detection logic

--- app/middleware/trailing-slash.global.ts
+++ app/middleware/trailing-slash.global.ts
@@ -1,23 +1,34 @@
 /**
- * Removes trailing slashes from URLs.
+ * Adds trailing slashes to URLs.
  *
- * This middleware only runs in development to maintain consistent behavior.
- * In production, Vercel handles this redirect via vercel.json.
+ * We use middleware to correctly handle all the cases, since Vercel doesn't handle it properly for nuxt app.
  *
- * - /package/vue/ → /package/vue
- * - /docs/getting-started/?query=value → /docs/getting-started?query=value
+ * - /package/vue → /package/vue/
+ * - /docs/getting-started?query=value → /docs/getting-started/?query=value
  */
 export default defineNuxtRouteMiddleware(to => {
-  if (import.meta.prerender) return
+  // request is marked as prerender for build and server ISR
+  // ignore rewrite for build prerender only
+  if (import.meta.prerender && process.env.NUXT_STAGE === 'build') return
+
+  // ignore for package-code (file viewer shouldn't relate on trailing slash) and api routes
+  if (
+    to.path.startsWith('/package-code/') ||
+    to.path.startsWith('/api/') ||
+    to.path.endsWith('/_payload.json')
+  )
+    return
 
   if (import.meta.server) {
     const event = useRequestEvent()
-    const url = event?.node.req.originalUrl || event?.node.req.url || ''
 
-    if (url.includes('/_payload.json')) return
+    // requests to /page-path/_payload.json are handled with to.path="/page-path", so we use the original url to check properly
+    const urlRaw = event?.node.req.originalUrl || event?.node.req.url || ''
+    const originalUrl = new URL(urlRaw, 'http://npmx.dev')
+
+    if (originalUrl.pathname.includes('/_payload.json') || originalUrl.pathname.endsWith('/'))
+      return
   }
-
-  if (to.path.startsWith('/package-code/') || to.path.startsWith('/api/')) return
 
   if (to.path !== '' && !to.path.endsWith('/')) {
     return navigateTo(
