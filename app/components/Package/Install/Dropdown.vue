81c7475d86e84fd95f23c2f63e7b619b03c20628
Vordgi <sasha2822222@gmail.com>
2026-09-20 13:13:18 +0100

    chore: hydration check

--- app/components/Package/Install/Dropdown.vue
+++ app/components/Package/Install/Dropdown.vue
@@ -58,6 +58,25 @@ const appSettings = useSettings()
 const panelId = useId()
 const isOpen = shallowRef(false)
 
+onPrehydrate(() => {
+  const settings = JSON.parse(localStorage.getItem('npmx-settings') || '{}')
+  if (settings?.installCommandsExpanded) {
+    document.documentElement.setAttribute('data-install-expanded', 'true')
+  }
+  console.log(
+    'onPrehydrate',
+    settings?.installCommandsExpanded,
+    document.documentElement.dataset.installExpanded,
+  )
+})
+
+onMounted(() => {
+  if (document.documentElement) {
+    isOpen.value = document.documentElement.dataset.installExpanded === 'true'
+  }
+  console.log('onMounted', document.documentElement.dataset.installExpanded, isOpen.value)
+})
+
 function toggle() {
   appSettings.settings.value.installCommandsExpanded =
     !appSettings.settings.value.installCommandsExpanded
@@ -77,19 +96,27 @@ function toggle() {
   )
 }
 
-onMounted(() => {
-  if (document.documentElement) {
-    isOpen.value = document.documentElement.dataset.installExpanded === 'true'
-  }
-  console.log('onMounted', document.documentElement.dataset.installExpanded, isOpen.value)
+watch(isOpen, newVal => {
+  console.log('watch isOpen', newVal)
 })
 
-onPrehydrate(() => {
-  const settings = JSON.parse(localStorage.getItem('npmx-settings') || '{}')
-  if (settings?.installCommandsExpanded) {
-    document.documentElement.setAttribute('data-install-expanded', 'true')
-  }
+watch(installParts, newVal => {
+  console.log('watch installParts', newVal)
 })
+
+watch(
+  () => appSettings.settings.value.installCommandsExpanded,
+  newVal => {
+    console.log('watch installCommandsExpanded', newVal)
+  },
+)
+
+watch(
+  () => selectedPM.value,
+  newVal => {
+    console.log('watch selectedPM', newVal)
+  },
+)
 </script>
 
 <template>
